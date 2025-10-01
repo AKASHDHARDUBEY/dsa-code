b276885db23ab9c93d6b4d6f11b02d0cddfe41cd
AKASHDHARDUBEY <akashdhar2112@gmail.com>
2025-10-02 02:46:18 +0530

    Bubble ,merge short , selection sort

---
+++ python/Selection_Sort.py
@@ -0,0 +1,16 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]: 
+                min_index = j
+        if i != min_index:  
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+
+# Example usage
+numbers = [64, 25, 12, 22, 11]
+sorted_numbers = selection_sort(numbers)
+print(sorted_numbers)
